60da77bf71ab4bb3325b56274b88e94f3af187a9
NotGrafik <pablo.jeanlouis@ynov.com>
2025-05-16 10:31:19 +0200

    :sparkles: Retrieving input values on register to post it and Add an eye button to see/hide his password

--- front/src/pages/RegisterPage.vue
+++ front/src/pages/RegisterPage.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 export const description =
   'A sign up form with first name, last name, email and password inside a card. There\'s an option to sign up with GitHub and a link to login if you already have an account';
 export const iframeHeight = '600px';
@@ -6,17 +6,37 @@ export const containerClass =
   'w-full h-screen flex items-center justify-center px-4';
 </script>
 
-<script setup>
-import { Button } from '@/components/ui/button';
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Eye } from 'lucide-vue-next'
+import { EyeClosed } from 'lucide-vue-next'
+
+// Champs liés au formulaire
+const firstName = ref('')
+const lastName = ref('')
+const email = ref('')
+const password = ref('')
+const showPassword = ref(false)
+
+const handleSubmit = () => {
+  console.log('Form submitted with:')
+  console.log({
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    password: password.value,
+  })
+}
 </script>
 
 <template>
@@ -31,28 +51,44 @@ import { Label } from '@/components/ui/label';
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div class="grid gap-4">
+        <form class="grid gap-4" @submit.prevent="handleSubmit">
           <div class="grid grid-cols-2 gap-4">
             <div class="grid gap-2">
               <Label for="first-name">First name</Label>
-              <Input id="first-name" placeholder="Max" required />
+              <Input id="first-name" v-model="firstName" placeholder="Max" required />
             </div>
             <div class="grid gap-2">
               <Label for="last-name">Last name</Label>
-              <Input id="last-name" placeholder="Robinson" required />
+              <Input id="last-name" v-model="lastName" placeholder="Robinson" required />
             </div>
           </div>
           <div class="grid gap-2">
             <Label for="email">Email</Label>
-            <Input id="email" type="email" placeholder="m..example.com" required />
+            <Input id="email" v-model="email" type="email" placeholder="m@example.com" required />
           </div>
           <div class="grid gap-2">
             <Label for="password">Password</Label>
-            <Input id="password" type="password" />
+            <div class="relative w-full max-w-sm">
+              <Input
+                id="password"
+                :type="showPassword ? 'text' : 'password'"
+                v-model="password"
+                placeholder="••••"
+                class="pr-10"
+                required
+              />
+              <span
+                class="absolute end-0 inset-y-0 flex items-center justify-center px-2 cursor-pointer"
+                @click="showPassword = !showPassword"
+              >
+                <EyeClosed v-if="!showPassword" class="size-5 text-muted-foreground" />
+                <Eye v-if="showPassword" class="size-5 text-muted-foreground" />
+              </span>
+            </div>
           </div>
           <Button type="submit" class="w-full"> Create an account </Button>
-          <Button variant="outline" class="w-full"> Sign up with GitHub </Button>
-        </div>
+          <Button variant="outline" class="w-full" type="button"> Sign up with GitHub </Button>
+        </form>
         <div class="mt-4 text-center text-sm">
           Already have an account?
           <a href="#" class="underline"> Sign in </a>
