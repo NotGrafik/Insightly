901b17a5e30580fbe6bd0305ba17ad902f235ce4
NotGrafik <pablo.jeanlouis@ynov.com>
2025-06-03 18:13:52 +0200

    ✨ Refactor HomePage and MySurveyPage components to utilize PageTemplate for consistent layout

--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -1,21 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import PageTemplate from './PageTemplate.vue';
 import SurveyList from '@/components/SurveyList.vue';
-import AppSidebar from '@/components/AppSidebar.vue';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar';
 
 const data = ref(null);
 
@@ -38,29 +24,8 @@ onMounted(() => {
 </script>
 
 <template>
-  <SidebarProvider>
-    <AppSidebar />
-    <SidebarInset>
-      <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-        <div class="flex items-center gap-2 px-4">
-          <SidebarTrigger class="-ml-1" />
-          <Separator orientation="vertical" class="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbSeparator class="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Home</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </header>
-      <div class="flex items-center gap-2 px-4">
-        <SurveyList v-if="data" :SurveyList="data" />
-        <div v-else>
-          <span>Loading...</span>
-        </div>
-      </div>
-    </SidebarInset>
-  </SidebarProvider>
+  <PageTemplate pageInfos="Home" >
+      <SurveyList v-if="data" :SurveyList="data" />
+      <div v-else class="text-center text-gray-500">Loading surveys...</div>
+  </PageTemplate>
 </template>
